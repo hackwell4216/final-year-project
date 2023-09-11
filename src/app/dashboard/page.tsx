9ec37c0a59b1d783a9620dfd44af94716e11029d
hackwell <hackwell.4216@gmail.com>
2023-09-11 06:55:48 +0000

    Initial Setup, Adding homescreen and navbar to dashboard

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,13 @@
+"use client"
+import React from "react"
+import GroupBuy from "@/app/dashboard/groupBuy";
+import SideNavbar from "@/app/dashboard/SideNavbar";
+
+export default function Dashboard(){
+    return (
+        <div>
+            <SideNavbar />
+            <GroupBuy />
+        </div>
+    )
+}''
